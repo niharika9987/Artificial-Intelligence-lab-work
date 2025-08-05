717db7a5aaa73bdde83f0f4129386e68851ec8af
niharika9987 <niharika10092005@gmail.com>
2025-08-05 23:36:23 +0530

    Add files via upload

---
+++ 8puzzle_bfs.cpp
@@ -0,0 +1,125 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<set>
+using namespace std;
+
+struct state {
+    int id;
+    int parent;
+    vector<vector<int>> current_state = vector<vector<int>>(3, vector<int>(3));
+};
+
+vector<vector<int>> Board_goal = {
+    {1, 2, 3},
+    {4, 5, 6},
+    {7, 8, 0}
+};
+
+void print_board(vector<vector<int>> board)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+bool GoalTest(state s)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (s.current_state[i][j] != Board_goal[i][j])
+                return false;
+        }
+    }
+    return true;
+}
+
+void BFS(state start)
+{
+    queue<state> q;
+    set<vector<vector<int>>> visited;
+    int id_counter = 0;
+
+    start.id = id_counter++;
+    start.parent = -1;
+    q.push(start);
+
+    while (!q.empty())
+    {
+        state current = q.front();
+        q.pop();
+
+        if (visited.count(current.current_state) != 0)
+            continue;
+
+        visited.insert(current.current_state);
+
+        if (GoalTest(current))
+        {
+            print_board(current.current_state);
+            return;
+        }
+
+        int x = -1, y = -1;
+        for (int i = 0; i < 3 && x == -1; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (current.current_state[i][j] == 0)
+                {
+                    x = i;
+                    y = j;
+                    break;
+                }
+            }
+        }
+
+        int dx[4] = {-1, 1, 0, 0};
+        int dy[4] = {0, 0, -1, 1};
+
+        for (int d = 0; d < 4; d++)
+        {
+            int new_x = x + dx[d];
+            int new_y = y + dy[d];
+
+            if (new_x >= 0 && new_y >= 0 && new_x < 3 && new_y < 3)
+            {
+                state new_state;
+                new_state.current_state = current.current_state;
+                int temp = new_state.current_state[new_x][new_y];
+                new_state.current_state[new_x][new_y] = new_state.current_state[x][y];
+                new_state.current_state[x][y] = temp;
+
+                if (visited.count(new_state.current_state) == 0)
+                {
+                    new_state.id = id_counter++;
+                    new_state.parent = current.id;
+                    q.push(new_state);
+                }
+            }
+        }
+    }
+}
+
+int main()
+{
+    state start;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cin >> start.current_state[i][j];
+        }
+    }
+
+    BFS(start);
+
+    return 0;
+}
